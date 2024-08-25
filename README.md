2f22aa7d7d986bcd4aa61df761af00398d517901
Asher Sandbach <ajsandbach@gmail.com>
2024-08-25 16:36:10 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# BSL Anki Deck
+
+This is an [Anki](https://docs.ankiweb.net) deck of British Sign Language (BSL) signs, based on the [GCSE vocabulary](https://www.signbsl.com/gcse-vocabulary) section of [SignBSL.com](https://www.signbsl.com/).
+
+If all you need is the deck itself, that can be found at <./BSL GCSE.apkg>.
+
+## How it works
+
+Requirements:
+
+- A Python interpreter
+- FFmpeg
+- Anki desktop
+
+The main Python script is a web scraper which follows each link on the GCSE Vocabulary page and downloads the information for one Anki note for each of the headings (`h1` or `h2`) on the page. In the interest of simplicity, the video URL that it chooses is always that of the first video that follows the heading. For example, on the page for the word [finish](https://www.signbsl.com/sign/finish), the script finds a note for each of the three senses, and chooses the first videos by SignStation, the University of Wolverhampton, and SignStation, respectively. It downloads and compresses the video, placing it in the [appropriate directory](https://docs.ankiweb.net/importing/text-files.html#importing-media).
+
+There is a great deal of variety within BSL, and this is reflected in the number of different signs shown for a single English word on SignBSL.com. For this reason, the video chosen for a particular sign by the script may not be one you have been taught. See below for how to add a different video to a note.
+
+Anki supports importing notes en masse with CSV files. The script writes to a CSV file according to the requirements described in the [Anki documentation](https://docs.ankiweb.net/importing/text-files.html), where the fields are as follows:
+
+1. Headword
+2. Definition
+3. Example
+4. Video (a filename based on the video URL)
+5. VideoURL
+6. VideoTitle
+7. URL
+8. Tags
+
+### Video compression
+
+While the videos on SignBSL.com are already fairly small and well-compressed, the script uses FFmpeg to compress them further, in the interest of minimizing the size of the final deck. The incantation used is as follows:
+
+``` shell
+ffmpeg -i "input.mp4" -vcodec libx265 -crf 32 "output.mp4"
+```
+
+The larger the number following `-i`, the greater the rate of compression.
+
+### Word frequency
+
+The notes in the deck ought to be in order of frequency, so that users see the most common signs first. I do not have data on the frequency of signs in BSL, so I am using English word frequency data as a proxy. I found an Excel spreadsheet of the 5000 most frequent lemmas in English at <www.wordfrequency.info>, converted the relevant sheet to tab-separated values with VisiData, and used Awk to obtain a [file of words](frequency.txt) and their frequency rankings.
+
+## Possible modifications
+
+If the Anki `collection.media` file you want to use is not found under the default `User 1`, you will have to amend <./script.py> to reflect your desired filepath.
+
+To add a card, use the function `get_definition` in the Python script to scrape the relevant information from a page on SignBSL.com, and print the resulting note or notes to a CSV file. Run `download_videos` on the CSV file to download and compress the first video for each definition. You can then import the CSV file into Anki, making sure that all the fields correspond as described above, and that 'Allow HTML in fields' is selected.
+
+To change the video associated with a particular card, find the URL of the video you want and modify the relevant part of the CSV file. Then, use `download_videos` as described above.
+
+In terms of file size, the deck is already fairly small. If you want to make it extremely tiny, at the cost of needing to download the videos when they appear for review, you could eschew the `download_videos` step and replace `{{Video}}` in the Anki card templates with the following:
+
+``` html
+<iframe src={{VideoURL}}></iframe>
+```
---
+++ script.py
@@ -0,0 +1,154 @@
+import requests
+from pathlib import Path
+from os import system
+from os.path import exists
+import re
+import csv
+from bs4 import BeautifulSoup
+
+CSV_PATH = "biglist.csv"
+ANKI_MEDIA = ".local/share/Anki2/User 1/collection.media/"
+
+
+class Note:
+    def __init__(self, *args):
+        args = args[0]
+        self.headword = args[0]
+        self.definition = args[1]
+        self.example = args[2]
+        self.video_url = args[3]
+        self.video_title = args[4]
+        self.url = args[5]
+        self.tags = [normalize_tag(x) for x in args[6]]
+
+    def __str__(self):
+        tag_str = " ".join(self.tags)
+
+        joined = ";".join(
+            [
+                normalize_csv(self.headword),
+                normalize_csv(self.definition),
+                normalize_csv(self.example),
+                f"[sound:{video_filename(self.video_url)}]",
+                normalize_csv(self.video_url),
+                normalize_csv(self.video_title),
+                normalize_csv(self.url),
+                normalize_csv(tag_str),
+            ]
+        )
+
+        return joined
+
+
+def get_page(url):
+    html = requests.get(url).text
+    page = BeautifulSoup(html, "lxml")
+    return page
+
+
+def get_definitions(url, tags):
+    page = get_page(url)
+    notes = []
+    headings = [page.find("h1")] + page.find_all("h2")
+    if not page.find_all(itemprop="video"):
+        return notes
+    for heading in headings:
+        headword = heading.text
+        tags += [x.text for x in heading.find_next_siblings("span")]
+        first_p = heading.find_next_sibling("p")
+        def_string = re.findall("</b> (.*?)<br/>", str(first_p))
+        if def_string:
+            definition = def_string[0]
+        else:
+            definition = ""
+        italics = first_p.find("i")
+        if italics is not None:
+            example = italics.text
+        else:
+            example = ""
+        video_div = first_p.find_next(itemprop="video")
+        video_url = video_div.find(itemprop="contentURL")["content"]
+        video_title = re.findall("(<i>.*?) <br/>", str(video_div))[0]
+        notes.append(
+            Note([headword, definition, example, video_url, video_title, url, tags])
+        )
+    return notes
+
+
+def normalize_tag(string):
+    return string.replace(" ", "_")
+
+
+def normalize_csv(string):
+    doubled_quotes = string.replace('"', '""')
+    return f'"{doubled_quotes}"'
+
+
+def video_filename(url):
+    end = re.findall("signbsl.com/(.*)", url)[0]
+    return end.replace("/", "_")
+
+
+def frequency(word):
+    word_file = "frequency.txt"
+    reg = re.compile(f"^(\\d+) {word}$", re.MULTILINE)
+    with open(word_file, "r") as file:
+        filetext = file.read()
+    results = reg.findall(filetext)
+    if results:
+        return int(results[0])
+    return 10000
+
+
+def word_list():
+    page = get_page("https://www.signbsl.com/gcse-vocabulary")
+    notes = []
+    for category in page.find_all("h3"):
+        pages = [
+            "https://signbsl.com" + x["href"]
+            for x in category.find_parent().find_all("a")
+        ]
+        for page in pages:
+            print("Contacting ", page)
+            notes += get_definitions(page, [category.text])
+    write_csv(CSV_PATH, notes)
+
+
+def write_csv(filename, notes):
+    notes.sort(key=lambda note: frequency(note.headword))
+    with open(filename, "w") as file:
+        file.writelines([str(note) + "\n" for note in notes])
+
+
+def convert_video(url):
+    temp = Path("/tmp/")
+    filename = video_filename(url)
+    dest = Path.home() / Path(ANKI_MEDIA)
+    if exists(dest / filename):
+        print("Already converted", dest / filename)
+        return
+    response = requests.get(url)
+    with open(f"{temp / filename}", mode="wb") as file:
+        file.write(response.content)
+    command = (
+        f'ffmpeg -i "{temp / filename}" -vcodec libx265 -crf 32 "{dest / filename}"'
+    )
+    system(command)
+
+
+def download_videos(csv_path):
+    notes = read_csv(csv_path)
+    for note in notes:
+        convert_video(note.video_url)
+
+
+def read_csv(path):
+    notes = []
+    with open(path) as file:
+        reader = csv.reader(file, delimiter=";")
+        for row in reader:
+            tags = row[7].split(" ")
+            # cut out the fourth item (see Note __str__ function)
+            row = row[0:3] + row[4:7] + [tags]
+            notes.append(Note(row))
+    return notes
